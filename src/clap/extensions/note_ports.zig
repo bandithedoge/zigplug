1ca76826ab01173b0b8051d6229bb2142f021042
bandithedoge <bandithedoge@protonmail.com>
2025-11-04 14:24:07 +0100

    fix note ports optional value

--- src/clap/extensions/note_ports.zig
+++ src/clap/extensions/note_ports.zig
@@ -14,12 +14,12 @@ pub fn extension(comptime Plugin: type) *const c.clap_plugin_note_ports_t {
 
     const note_ports = struct {
         pub fn count(_: [*c]const c.clap_plugin_t, is_input: bool) callconv(.c) u32 {
-            const ports = if (is_input) meta_ports.?.in else meta_ports.?.out;
+            const ports = if (is_input) meta_ports.in else meta_ports.out;
             return @intCast(ports.len);
         }
 
         pub fn get(_: [*c]const c.clap_plugin_t, index: u32, is_input: bool, info: [*c]c.clap_note_port_info_t) callconv(.c) bool {
-            const ports = if (is_input) meta_ports.?.in else meta_ports.?.out;
+            const ports = if (is_input) meta_ports.in else meta_ports.out;
 
             if (index >= ports.len) {
                 return false;
