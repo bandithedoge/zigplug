f12dc24528b7f794d4256314435f08aac71b4ffb
bandithedoge <bandithedoge@protonmail.com>
2025-11-07 13:39:43 +0100

    use std.Io.{Reader,Writer} for clap streams

--- src/clap/extensions/state.zig
+++ src/clap/extensions/state.zig
@@ -10,22 +10,87 @@ const clap = @import("clap");
 const std = @import("std");
 const log = std.log.scoped(.zigplug_clap_state);
 
+// TODO: make Writer and Reader buffered
+
+pub const Writer = struct {
+    clap_stream: *const c.clap_ostream,
+    writer: std.Io.Writer,
+
+    pub fn init(clap_stream: *const c.clap_ostream) Writer {
+        return .{
+            .clap_stream = clap_stream,
+            .writer = .{
+                .vtable = &.{
+                    .drain = drain,
+                },
+                .buffer = &.{},
+            },
+        };
+    }
+
+    fn drain(writer: *std.Io.Writer, data: []const []const u8, _: usize) std.Io.Writer.Error!usize {
+        const self: *Writer = @fieldParentPtr("writer", writer);
+        var total_written: usize = 0;
+        for (data) |bytes| {
+            const written = self.clap_stream.write.?(self.clap_stream, bytes.ptr, bytes.len);
+            if (written == -1)
+                return error.WriteFailed;
+            total_written += @abs(written);
+        }
+        return total_written;
+    }
+};
+
+const Reader = struct {
+    clap_stream: *const c.clap_istream,
+    reader: std.Io.Reader,
+
+    pub fn init(clap_stream: *const c.clap_istream) Reader {
+        return .{
+            .clap_stream = clap_stream,
+            .reader = .{
+                .vtable = &.{
+                    .stream = stream,
+                },
+                .buffer = &.{},
+                .seek = 0,
+                .end = 0,
+            },
+        };
+    }
+
+    fn stream(reader: *std.Io.Reader, writer: *std.Io.Writer, limit: std.Io.Limit) std.Io.Reader.StreamError!usize {
+        const self: *Reader = @fieldParentPtr("reader", reader);
+        while (true) {
+            const buffer = try writer.writableSliceGreedy(limit.toInt() orelse 64);
+            switch (self.clap_stream.read.?(self.clap_stream, buffer.ptr, buffer.len)) {
+                -1 => return error.ReadFailed,
+                0 => return error.EndOfStream,
+                else => |read| {
+                    writer.advance(@abs(read));
+                    return @abs(read);
+                },
+            }
+        }
+    }
+};
+
 pub fn extension(comptime _: type) *const c.clap_plugin_state {
     const state = struct {
         pub fn save(clap_plugin: [*c]const c.clap_plugin, stream: [*c]const c.clap_ostream) callconv(.c) bool {
             const data = clap.Data.fromClap(clap_plugin);
+            var clap_writer = Writer.init(stream);
+            const writer = &clap_writer.writer;
 
             for (data.plugin.parameters.?.slice) |parameter| {
                 switch (parameter.*) {
                     inline else => |p| {
                         const value = p.get();
-                        const size = @sizeOf(@TypeOf(value));
-                        var buffer = std.mem.toBytes(&value);
-                        const bytes = stream.*.write.?(stream, @ptrCast(&buffer), size);
-                        if (bytes != size) {
+                        const bytes = std.mem.asBytes(&value);
+                        writer.writeAll(bytes) catch {
                             log.err("failed to save parameter '{s}' = {}", .{ p.options.name, value });
                             return false;
-                        }
+                        };
                         log.debug("saved parameter '{s}' = {any}", .{ p.options.name, value });
                     },
                 }
@@ -36,17 +101,17 @@ pub fn extension(comptime _: type) *const c.clap_plugin_state {
 
         pub fn load(clap_plugin: [*c]const c.clap_plugin, stream: [*c]const c.clap_istream) callconv(.c) bool {
             const data = clap.Data.fromClap(clap_plugin);
+            var clap_reader = Reader.init(stream);
+            const reader = &clap_reader.reader;
 
             for (data.plugin.parameters.?.slice) |parameter| switch (parameter.*) {
                 inline else => |*p| {
                     var value = p.get();
-                    const size = @sizeOf(@TypeOf(value));
-                    var buffer = std.mem.asBytes(&value);
-                    const bytes = stream.*.read.?(stream, @ptrCast(&buffer), size);
-                    if (bytes != size) {
+                    const buffer = std.mem.asBytes(&value);
+                    reader.readSliceAll(buffer) catch {
                         log.err("failed to load parameter '{s}'", .{p.options.name});
                         return false;
-                    }
+                    };
                     p.set(value);
 
                     log.debug("loaded parameter '{s}' = {any}", .{ p.options.name, value });
